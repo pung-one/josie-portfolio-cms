413c3de4a36d5a89be179137f3728369d58a264c
pung-one <p-ungerer@gmx.net>
2023-06-17 18:20:34 +0200

    add some content types

---
+++ src/api/sounddesign/routes/sounddesign.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sounddesign router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::sounddesign.sounddesign');
